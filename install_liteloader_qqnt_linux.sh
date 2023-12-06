5f1117370b9711c1a5040c961355e5039d91a611
soloxiaoye2022 <soloxiaoye2022@163.com>
2023-12-06 13:17:02 +0800

    优化安装逻辑

--- install_liteloader_qqnt_linux.sh
+++ install_liteloader_qqnt_linux.sh
@@ -161,8 +161,8 @@ TRSS_Yunzai_install() {
     git clone --depth 1 ${ghproxy}https://github.com/yoimiya-kokomi/miao-plugin plugins/miao-plugin
     git clone --depth 1 ${ghproxy}https://github.com/TimeRainStarSky/TRSS-Plugin plugins/TRSS-Plugin
     git clone -b red ${ghproxy}https://github.com/xiaoye12123/ws-plugin.git ./plugins/ws-plugin
-    npm install -g pnpm@8.11.0 && pnpm i || echo -e "${Error} Yunzai 安装失败,请反馈..." && exit 1
-    nohup node app > /dev/null 2>&1 & #生成配置文件
+    npm install -g pnpm@8.11.0 && pnpm i
+    nohup node app > /dev/null 2>&1  #生成配置文件
     node_pid=$!
     set_bot_qq
     set_master_qq
